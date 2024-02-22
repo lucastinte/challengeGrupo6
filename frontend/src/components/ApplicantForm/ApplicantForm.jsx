fd440b39d09c9b1cf44c783450e23b29a3eabcc2
lepzito <lucasrtinte19@gmail.com>
2024-02-22 06:51:48 -0300

    css corregido mobile

--- frontend/src/components/ApplicantForm/ApplicantForm.jsx
+++ frontend/src/components/ApplicantForm/ApplicantForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { getProfesions, createApplicant } from '../../services/serviceApi';
-const ApplicantForm = ({ handleSubmit }) => {
+const ApplicantForm = () => {
   const navigate = useNavigate();
   const [dni, setDni] = useState('');
   const [name, setName] = useState('');
